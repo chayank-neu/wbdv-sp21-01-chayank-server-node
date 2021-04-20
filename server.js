e659256fd9a897e1b8ddf63add3c2a36318d408d
Chayank11 <chayank.tm@gmail.com>
2021-04-20 01:12:36 -0400

    QUIZ NODE SERVER CODE

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/whiteboard',
-    {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+    "mongodb+srv://chayank:123!pass@cluster0.0ssbw.mongodb.net/whiteboard?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://chayank:123!pass@cluster0.0ssbw.mongodb.net/whiteboard?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
